42da9eeb2ca0cdd0f680700eeb41484332382200
KiraVill <61442983+KiraVill@users.noreply.github.com>
2021-05-14 13:47:54 +1000

    Add best GEBV calculator + fix save_AM_pedigree
    Added new function calculate_optimal_gebv.
    Fixed bug in save_AM_pedigree where the call would fail because
    the get_parents_of_id call was using the parent AlleleMatrix not
    the one containing the children.

--- sim-operations.c
+++ sim-operations.c
@@ -4809,6 +4809,46 @@ char* calculate_ideal_genotype(SimData* d) {
 	return optimal;
 }
 
+/** Takes a look at the currently-loaded effect values returns the highest possible
+ * GEBV any genotype could score using those effect values.
+ *
+ * The SimData must be initialised with marker effects for this function to succeed.
+ * 
+ * @param d pointer to the SimData containing markers and marker effects.
+ * @returns the GEBV of the best/ideal genotype.
+ */
+double calculate_optimal_gebv(SimData* d) {
+	char* best_alleles = calculate_ideal_genotype(d);
+	double best_gebv = 0;
+	
+	DecimalMatrix counts = generate_zero_dmatrix(d->n_markers, 1);
+	DecimalMatrix effect_row, product;
+	
+	for (int i = 0; i < d->e.effects.rows; ++i) {
+		// fill the count matrix for this allele.
+		for (int j = 0; j < d->n_markers; ++j) {
+			if (best_alleles[j] == d->e.effect_names[i]) {
+				counts.matrix[j][0] = 2;
+			} else {
+				counts.matrix[j][0] = 0;
+			}
+		}
+		
+		// calculate the GEBV contribution from this allele
+		effect_row = subset_dmatrix_row(&(d->e.effects), i);
+		product = multiply_dmatrices(&effect_row, &counts);
+		
+		best_gebv += product.matrix[0][0];
+		delete_dmatrix(&effect_row);
+		delete_dmatrix(&product);
+	}
+	
+	delete_dmatrix(&counts);
+	free(best_alleles);
+	
+	return best_gebv;
+}
+
 /*--------------------------------Printing-----------------------------------*/
 
 /** Prints the setup data (everything except the actual genotypes) stored inside
@@ -5288,6 +5328,7 @@ void save_group_full_pedigree(FILE* f, SimData* d, int group) {
 	unsigned int* group_contents = get_group_ids( d, group, group_size);
 	char** group_names = get_group_names( d, group, group_size);
 	const char newline[] = "\n";
+	unsigned int pedigree[2];
 	
 	for (int i = 0; i < group_size; i++) {
 		/*Group member name*/
@@ -5296,7 +5337,9 @@ void save_group_full_pedigree(FILE* f, SimData* d, int group) {
 			fwrite(group_names[i], sizeof(char), strlen(group_names[i]), f);
 		}
 		
-		save_parents_of(f, d->m, group_contents[i]);
+		if (get_parents_of_id(d->m, group_contents[i], pedigree) == 0) {
+			save_parents_of(f, d->m, pedigree[0], pedigree[1]);
+		}
 		fwrite(newline, sizeof(char), 1, f);
 	}
 	free(group_names);
@@ -5345,7 +5388,7 @@ void save_full_pedigree(FILE* f, SimData* d) {
 				fwrite(m->subject_names[i], sizeof(char), strlen(m->subject_names[i]), f);
 			}
 			
-			save_parents_of(f, d->m, m->ids[i]);
+			save_parents_of(f, d->m, m->pedigrees[0][i], m->pedigrees[1][i]);
 			fwrite(newline, sizeof(char), 1, f);
 		}
 	} while ((m = m->next) != NULL);
@@ -5386,7 +5429,7 @@ void save_full_pedigree(FILE* f, SimData* d) {
  */
 void save_AM_pedigree(FILE* f, AlleleMatrix* m, SimData* parents) {
 	const char newline[] = "\n";
-	
+
 	for (int i = 0; i < m->n_subjects; ++i) {
 		/*Group member name*/
 		fprintf(f, "%d\t", m->ids[i]);
@@ -5394,7 +5437,7 @@ void save_AM_pedigree(FILE* f, AlleleMatrix* m, SimData* parents) {
 			fwrite(m->subject_names[i], sizeof(char), strlen(m->subject_names[i]), f);
 		}
 		
-		save_parents_of(f, parents->m, m->ids[i]);
+		save_parents_of(f, parents->m, m->pedigrees[0][i], m->pedigrees[1][i]);
 		fwrite(newline, sizeof(char), 1, f);
 	}
 	fflush(f);
@@ -5422,54 +5465,66 @@ void save_AM_pedigree(FILE* f, AlleleMatrix* m, SimData* parents) {
  * @param id the session-unique id of the genotype whose parents 
  * we wish to recursively save.
  */
-void save_parents_of(FILE* f, AlleleMatrix* m, unsigned int id) {
+void save_parents_of(FILE* f, AlleleMatrix* m, unsigned int p1, unsigned int p2) {
 	unsigned int pedigree[2];
 	
-	if (get_parents_of_id(m, id, pedigree) == 0) {
-		// open brackets
-		fwrite("=(", sizeof(char), 2, f);
-		char* name;
-		
-		if (pedigree[0] == pedigree[1]) {
-			// Selfed parent
-			name = get_name_of_id( m, pedigree[0]);
-			if (name != NULL) {
-				fwrite(name, sizeof(char), strlen(name), f);
-			} else if (pedigree[0] > 0) {
-				fprintf(f, "%d", pedigree[0]);
-				//fwrite(pedigree, sizeof(int), 1, f);
-			}
-			save_parents_of(f, m, pedigree[0]);
-					
-		} else {
-			// Parent 1
-			name = get_name_of_id( m, pedigree[0]);
-			if (name != NULL) {
-				fwrite(name, sizeof(char), strlen(name), f);
-			} else if (pedigree[0] > 0) {
-				fprintf(f, "%d", pedigree[0]);
-				//fwrite(pedigree, sizeof(int), 1, f);
-			}
-			save_parents_of(f, m, pedigree[0]);
-			
-			// separator
-			fwrite(",", sizeof(char), 1, f);
-			
-			// Parent 2
-			name = get_name_of_id( m, pedigree[1]);
-			if (name != NULL) {
-				fwrite(name, sizeof(char), strlen(name), f);
-			} else if (pedigree[1] > 0) {
-				fprintf(f, "%d", pedigree[1]);
-				//fwrite(pedigree + 1, sizeof(int), 1, f);
-			}
-			save_parents_of(f, m, pedigree[1]);
-			
-		}
-
-		// close brackets
-		fwrite(")", sizeof(char), 1, f);
+	// open brackets
+	fwrite("=(", sizeof(char), 2, f);
+	char* name;
+	
+	// enables us to print only the known parent if one is unknown
+	if (p1 == 0 || p2 == 0) {
+		p1 = (p1 >= p2) ? p1 : p2; //max of the two
+		p2 = p1;
 	}
+	
+	if (p1 == p2) {
+		if (p1 > 0) { //print nothing if both are unknown.
+			// Selfed parent
+			name = get_name_of_id( m, p1);
+			if (name != NULL) {
+				fwrite(name, sizeof(char), strlen(name), f);
+			} else if (p1 > 0) {
+				fprintf(f, "%d", p1);
+				//fwrite(pedigree, sizeof(int), 1, f);
+			}
+			
+			if (get_parents_of_id(m, p1, pedigree) == 0) {
+				save_parents_of(f, m, pedigree[0], pedigree[1]);
+			}
+		}
+	} else {
+		// Parent 1
+		name = get_name_of_id( m, p1);
+		if (name != NULL) {
+			fwrite(name, sizeof(char), strlen(name), f);
+		} else if (p1 > 0) {
+			fprintf(f, "%d", p1);
+			//fwrite(pedigree, sizeof(int), 1, f);
+		}
+		get_parents_of_id(m, p1, pedigree);
+		save_parents_of(f, m, pedigree[0], pedigree[1]);
+		
+		// separator
+		fwrite(",", sizeof(char), 1, f);
+		
+		// Parent 2
+		name = get_name_of_id( m, p2);
+		if (name != NULL) {
+			fwrite(name, sizeof(char), strlen(name), f);
+		} else if (p2 > 0) {
+			fprintf(f, "%d", p2);
+			//fwrite(pedigree + 1, sizeof(int), 1, f);
+		}
+		
+		if (get_parents_of_id(m, p2, pedigree) == 0) {
+			save_parents_of(f, m, pedigree[0], pedigree[1]);
+		}
+		
+	}
+
+	// close brackets
+	fwrite(")", sizeof(char), 1, f);
 }
 
 /** Print the GEBV of each genotype in a group to a file. The following
--- sim-operations.h
+++ sim-operations.h
@@ -386,7 +386,7 @@ void save_one_step_pedigree(FILE* f, SimData* d);
 void save_group_full_pedigree(FILE* f, SimData* d, int group);
 void save_full_pedigree(FILE* f, SimData* d);
 void save_AM_pedigree(FILE* f, AlleleMatrix* m, SimData* parents);
-void save_parents_of(FILE* f, AlleleMatrix* m, unsigned int id);
+void save_parents_of(FILE* f, AlleleMatrix* m, unsigned int p1, unsigned int p2);
 
 void save_group_fitness(FILE* f, SimData* d, int group);
 void save_fitness(FILE* f, DecimalMatrix* e, unsigned int* ids, char** names);
@@ -396,4 +396,5 @@ void save_count_matrix(FILE* f, SimData* d, char allele);
 void save_count_matrix_of_group(FILE* f, SimData* d, char allele, int group);
 
 char* calculate_ideal_genotype(SimData* d);
+double calculate_optimal_gebv(SimData* d);
 #endif
