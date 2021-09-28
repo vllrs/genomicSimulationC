1c1333a6e664fcad755b1cfe3a0a2a87e26b8e9c
KiraVill <61442983+KiraVill@users.noreply.github.com>
2021-09-28 11:59:38 +1000

    Prune old docs pages

--- docs/html/search/defines_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['tol_392',['TOL',['../sim-operations_8h.html#a156b862ebf6d213f5da19b9e3ccb779e',1,'sim-operations.h']]],
-  ['true_393',['TRUE',['../sim-operations_8h.html#aa8cecfc5c5c054d2875c03e77b7be15d',1,'sim-operations.h']]]
-];
--- docs/html/search/pages_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['template_2fsample_20workflow_20for_20using_20the_20tool_374',['Template/sample workflow for using the tool',['../_template.html',1,'']]]
-];
